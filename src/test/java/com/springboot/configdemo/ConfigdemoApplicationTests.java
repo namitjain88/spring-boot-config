d425bd465734c8b201decd23bb38cbc7a0fc49e6
Namit Jain <namit.jain88@gmail.com>
2020-02-17 16:50:33 -0600

    base spring boot web project

---
+++ src/test/java/com/springboot/configdemo/ConfigdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.configdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
