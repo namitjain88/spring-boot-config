f73eeffe13d161b4a44eb5c216eb7de45a09b3f9
Namit Jain <namit.jain88@gmail.com>
2020-02-18 14:55:22 -0600

    Injecting key-value pairs using @Value("  #{ _${key}_ } ") and keeping variable type as Map

--- src/main/java/com/springboot/configdemo/controllers/GreetingController.java
+++ src/main/java/com/springboot/configdemo/controllers/GreetingController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class GreetingController {
 
@@ -13,8 +15,11 @@ public class GreetingController {
     @Value("${app.description}")
     private String appDescription;
 
+    @Value("${my.list.values}")
+    private List<String> listValues;
+
     @GetMapping("/greeting")
     public String greeting(){
-        return greetingMessage + "\n" + appDescription;
+        return greetingMessage + "\n" + appDescription + "\n" + listValues;
     }
 }
